f2f65bf49535f7b48c26880a131c195085e9e397
Alex Kyriacou <a.kyriacou14@gmail.com>
2024-10-06 11:57:00 +1100

    add-resource-group-to-terraform

---
+++ terraform/maint.tf
@@ -0,0 +1,29 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurecaf_name" "resource_group" {
+  name          = var.application_name
+  resource_type = "azurerm_resource_group"
+  suffixes      = [var.environment]
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = azurecaf_name.resource_group.result
+  location = var.location
+}
+
+
---
+++ terraform/variables.tf
@@ -0,0 +1,16 @@
+variable "application_name" {
+  type        = string
+  description = "The name of your application"
+}
+
+variable "environment" {
+  type        = string
+  description = "The environment (dev, test, prod...)"
+  default     = ""
+}
+
+variable "location" {
+  type        = string
+  description = "The Azure region where all resources in this example should be created"
+  default     = "australiasoutheast"
+}
