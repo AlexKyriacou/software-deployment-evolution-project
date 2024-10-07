9a82ff8169ee28ad07718713b11c60b1cb518bcc
Marella Morad <marella99mourad@gmail.com>
2024-10-08 00:31:57 +1100

    Add Media Section

--- src/app/media/config.ts
+++ src/app/media/config.ts
@@ -24,4 +24,28 @@ export const mediaConfig: MediaConfig = {
       src: "https://www.youtube.com/embed/oDMh3a11Zg0?si=kjE8GcR_cvn_10jV",
     },
   ],
+  blogs: [
+    {
+      id: 1,
+      title: "What Is Gluten & Can It Be Part of a 'Healthy' Diet?",
+      src: "https://fourwellness.co/blog/what-is-gluten-can-it-be-part-of-a-healthy-diet",
+      image:
+        "https://images.squarespace-cdn.com/content/v1/5b0480935ffd20abd67339a8/1619047799233-31E5RGU487ULJHZ8NG7A/gluten.png?format=1000w",
+    },
+    {
+      id: 2,
+      title: "How to Build a Fulfilling Career Doing What You Love",
+      src: "https://fourwellness.co/blog/how-to-build-a-fulfilling-career-doing-what-you-love",
+      image:
+        "https://images.squarespace-cdn.com/content/v1/5b0480935ffd20abd67339a8/1617385824446-CVYDIVH8RN4RUV6JAC7D/Fulfilling+Career-min.png?format=1000w",
+    },
+    {
+      id: 3,
+      title:
+        "How Sugar Addiction Affects Your Body (& 7 Tips to Eat Less Sugar)",
+      src: "https://fourwellness.co/blog/how-sugar-addiction-affects-your-body-and-how-to-eat-less-sugar",
+      image:
+        "https://images.squarespace-cdn.com/content/v1/5b0480935ffd20abd67339a8/1617376848001-865IRDWBHGGID8DZXOMW/Sugar+Addiction.png?format=1000w",
+    },
+  ],
 };
--- src/app/media/page.tsx
+++ src/app/media/page.tsx
@@ -4,12 +4,52 @@ import { useState } from "react";
 import MainSection from "@/components/common/main-section";
 import MainSectionBody from "@/components/common/main-section-body";
 import Title from "@/components/common/title";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardTitle } from "@/components/ui/card";
 import Image from "next/image";
 import { mediaConfig } from "./config";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  DialogTitle,
+  DialogHeader,
+  DialogFooter,
+  DialogClose,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { toast } from "@/hooks/use-toast";
+import Link from "next/link";
 
 export default function MediaPage() {
   const [selectedVideoIndex, setSelectedVideoIndex] = useState(0);
+  const [selectedBlogIndex, setSelectedBlogIndex] = useState(0);
+  const [videoTitle, setVideoTitle] = useState("");
+  const [videoSrc, setVideoSrc] = useState("");
+  const [blogTitle, setBlogTitle] = useState("");
+  const [blogSrc, setBlogSrc] = useState("");
+  const [blogImage, setBlogImage] = useState("");
+
+  function addVideo() {
+    console.log("Title:", videoTitle);
+    console.log("Embedding URL:", videoSrc);
+    toast({ title: "Success", description: "Video added successfully" });
+    setVideoTitle("");
+    setVideoSrc("");
+    //TODO: Save Video to DB
+  }
+
+  function addBlog() {
+    console.log("Title:", blogTitle);
+    console.log("URL:", blogSrc);
+    console.log("Image:", blogImage);
+    toast({ title: "Success", description: "Blog added successfully" });
+    setBlogTitle("");
+    setBlogSrc("");
+    setBlogImage("");
+    //TODO: Save Blog to DB
+  }
 
   return (
     <div className="mx-auto mb-10 max-w-7xl">
@@ -17,7 +57,7 @@ export default function MediaPage() {
         <MainSectionBody>
           <Title>Media</Title>
           <Image
-            src="/man-on-laptop-youtube.svg"
+            src="/woman-on-laptop-google.svg"
             alt="About us"
             width={400}
             height={400}
@@ -25,10 +65,41 @@ export default function MediaPage() {
           />
         </MainSectionBody>
       </MainSection>
-      <Card className="mt-5 w-full">
-        <CardHeader>
+      <Card className="mt-5 w-full p-5">
+        <div className="flex flex-row items-center justify-between">
           <CardTitle className="text-xl">Useful Videos</CardTitle>
-        </CardHeader>
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button>Add New Video</Button>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Add New Video</DialogTitle>
+              </DialogHeader>
+              <Label htmlFor="title">Title</Label>
+              <Input
+                id="title"
+                type="text"
+                value={videoTitle}
+                onChange={(e) => setVideoTitle(e.target.value)}
+              />
+              <Label htmlFor="src">Embedding URL</Label>
+              <Input
+                id="src"
+                type="text"
+                value={videoSrc}
+                onChange={(e) => setVideoSrc(e.target.value)}
+              />
+              <DialogFooter className="sm:justify-start">
+                <DialogClose asChild>
+                  <Button type="button" variant="secondary" onClick={addVideo}>
+                    Submit
+                  </Button>
+                </DialogClose>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
+        </div>
         <div className="flex flex-col items-start py-4 md:flex-row">
           <CardContent className="md:w-1/3">
             {mediaConfig.videos.map((video, index) => (
@@ -57,6 +128,81 @@ export default function MediaPage() {
                     allowFullScreen
                   ></iframe>
                 </div>
+              )}
+          </CardContent>
+        </div>
+      </Card>
+      <Card className="mt-5 w-full p-5">
+        <div className="flex flex-row items-center justify-between">
+          <CardTitle className="text-xl">Latest Blogs</CardTitle>
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button>Add New Blog</Button>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Add New Blog</DialogTitle>
+              </DialogHeader>
+              <Label htmlFor="blogTitle">Title</Label>
+              <Input
+                id="blogTitle"
+                type="text"
+                value={blogTitle}
+                onChange={(e) => setBlogTitle(e.target.value)}
+              />
+              <Label htmlFor="blogSrc">URL</Label>
+              <Input
+                id="blogSrc"
+                type="text"
+                value={blogSrc}
+                onChange={(e) => setBlogSrc(e.target.value)}
+              />
+              <Label htmlFor="blogImage">Image</Label>
+              <Input
+                id="blogImage"
+                type="text"
+                value={blogImage}
+                onChange={(e) => setBlogImage(e.target.value)}
+              />
+              <DialogFooter className="sm:justify-start">
+                <DialogClose asChild>
+                  <Button type="button" variant="secondary" onClick={addBlog}>
+                    Submit
+                  </Button>
+                </DialogClose>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
+        </div>
+        <div className="flex flex-col items-start py-4 md:flex-row">
+          <CardContent className="md:w-1/3">
+            {mediaConfig.blogs.map((blog, index) => (
+              <div
+                key={blog.id}
+                className={`cursor-pointer p-5 ${
+                  selectedBlogIndex === index ? "bg-secondary" : ""
+                }`}
+                onClick={() => setSelectedBlogIndex(index)}
+              >
+                <p className="font-medium leading-none">{blog.title}</p>
+              </div>
+            ))}
+          </CardContent>
+          <CardContent className="md:w-2/3" style={{ margin: 0 }}>
+            {selectedBlogIndex !== null &&
+              mediaConfig.blogs[selectedBlogIndex] && (
+                <Link
+                  href={mediaConfig.blogs[selectedBlogIndex].src}
+                  target="_blank"
+                >
+                  <Image
+                    src={mediaConfig.blogs[selectedBlogIndex].image}
+                    alt={mediaConfig.blogs[selectedBlogIndex].title}
+                    width={900}
+                    height={500}
+                    className="w-full h-full object-cover"
+                  />
+                </Link>
               )}
           </CardContent>
         </div>
