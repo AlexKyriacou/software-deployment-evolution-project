a11d3158528d9f7c01598a1488218c3184218f06
Alex Kyriacou <a.kyriacou14@gmail.com>
2024-10-10 15:31:56 +1100

    feat: add app url to tf outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,3 +2,8 @@ output "resource_group" {
   value       = azurerm_resource_group.main.name
   description = "The resource group."
 }
+
+output "application_url" {
+  value       = module.application.application_url
+  description = "The Web application URL."
+}
